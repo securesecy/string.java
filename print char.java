6cb0de2314220571ad3e49bada303544c9588988
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-06-25 14:58:57 +0530

    print all characters of a string

---
+++ print char.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Strings{
+
+     public static void main(String []args){
+        Scanner sc = new Scanner (System.in);
+        
+        String FirstName = sc.next();
+        String LastName = sc.next();
+        String FullName = FirstName + " " + LastName;
+        System.out.println(FullName.length());
+        for(int i=0 ; i<FullName.length();i++){
+            System.out.println(FullName.charAt(i));
+        }
+     }
+}
